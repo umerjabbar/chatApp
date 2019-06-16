85dc5728deb91e40670855776318cfdd2563c11e
Umer Jabbar <umerabduljabbar@icloud.com>
2019-06-16 17:56:48 +0500

    Initial Commit

---
+++ ChatApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChatApp
+//
+//  Created by Umer Jabbar on 16/06/2019.
+//  Copyright © 2019 Ali Jabbar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
