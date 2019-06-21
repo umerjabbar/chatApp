a0e0373d312de3968818e9e2ad4c12c83f38db64
Umer Jabbar <umerabduljabbar@icloud.com>
2019-06-21 18:44:56 +0500

    creating group

--- ChatApp/View Controllers/Create Group/UsersViewController.swift
+++ ChatApp/View Controllers/Create Group/UsersViewController.swift
@@ -9,22 +9,64 @@
 import UIKit
 
 class UsersViewController: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return UIStatusBarStyle.lightContent
+    }
+    
+    var users = [MyUser]()
+    var selectedUsers = [MyUser]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        self.tableView.dataSource = self
+        self.tableView.delegate = self
+        
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func backbuttonAction(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
     }
-    */
+    
+    func getUsers(){
+        guard let rows = self.tableView.indexPathsForSelectedRows?.map({ (indexPath) -> Int in
+            return indexPath.row
+        })else{return}
+    }
+    
+}
 
+extension UsersViewController : UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 8
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "UsersTableViewCell", for: indexPath) as! UsersTableViewCell
+        return cell
+        
+    }
+    
+}
+
+extension UsersViewController : UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        guard let cell = tableView.cellForRow(at: indexPath) as? UsersTableViewCell else {return}
+        cell.selectView(select: true)
+    }
+    
+    func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
+        guard let cell = tableView.cellForRow(at: indexPath) as? UsersTableViewCell else {return}
+        cell.selectView(select: false)
+    }
+    
+    
+    
 }
